1d827b4efa86bfd49f2690126ffd3f7163a5572d
ImOrion <mateou771@gmail.com>
2022-11-24 09:52:04 -0500

    ahg

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,59 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
-// Escribí acá tu servidor
+// const protocolo = (req, res) => {
+//     let urls = req.url;
+//     urls = urls.slice(1);
+//     try {
+//         const myHtml = fs.readFileSync(`./images/${urls}_doge.jpg`);
+//         console.log(myHtml)
+//         console.log(`./images/${urls}_doge.jpg`);
+//         res.writeHead(200, { "Content-type": "image/jpeg" });
+//         res.end(myHtml);
+//     } catch (err) {
+//         res.writeHead(404, { "Content-type": "text/plain" });
+//         res.end('error lasdasdasddas');
+//     }
+
+// };
+// http.createServer(protocolo).listen(3000, "localhost");
+
+http
+  .createServer((req, res) => {
+    if (req.url === "/arcoiris_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/arcoiris_doge.jpg");
+      res.end(imagen);
+    }
+
+    if (req.url === "/badboy_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/badboy_doge.jpg");
+      res.end(imagen);
+    }
+    if (req.url === "/code_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/code_doge.jpg");
+      res.end(imagen);
+    }
+    if (req.url === "/resaca_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/resaca_doge.jpg");
+      res.end(imagen);
+    }
+    if (req.url === "/retrato_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/retrato_doge.jpg");
+      res.end(imagen);
+    }
+
+    if (req.url === "/sexy_doge") {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      const imagen = fs.readFileSync("./images/sexy_doge.jpg");
+      res.end(imagen);
+    } else {
+      res.writeHead(404, { "Content-type": "plain/text" });
+      res.end("error nout found");
+    }
+  })
+  .listen(3001, "localhost");
--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,12 +1,106 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+// class $Promise {
+//     constructor(executor) {
+//       if (typeof executor !== "function") {
+//         throw TypeError("executor function");
+//       }
 
+//       this._state = "pending";
+//     }
+
+//     _internalResolve() {
+//       this._state = "fulfilled";
+//     }
+//     _internalReject() {}
+//   }
+
+function $Promise(executor) {
+  if (typeof executor !== "function") {
+    throw new TypeError("executor is not a function");
+  }
+  this._state = "pending";
+  this._value = undefined;
+  this._handlerGroups = [];
+  executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+}
+
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  var object = {
+    successCb,
+    errorCb,
+  };
+
+  if (typeof successCb != "function") object.successCb = false;
+  if (typeof errorCb != "function") object.errorCb = false;
+  this._handlerGroups.push(object);
+
+  if (this._state !== "pending") {
+    this._callHandlers();
+  }
+};
+
+
+
+$Promise.prototype.catch = function (errorCb) {
+  this.then(null, errorCb);
+};
+
+
+
+$Promise.prototype._internalResolve = function (someData) {
+  if (this._state === "pending") {
+    this._value = someData;
+    this._state = "fulfilled";
+    this._callHandlers();
+  }
+};
+
+
+$Promise.prototype._internalReject = function (someData) {
+  if (this._state === "pending") {
+    this._value = someData;
+    this._state = "rejected";
+    this._callHandlers();
+  }
+};
+
+
+$Promise.prototype._callHandlers = function () {
+  while (this._handlerGroups.length > 0) {
+    var actualHandler = this._handlerGroups.shift();
+    if (this._state === "fulfilled") {
+      actualHandler.successCb && actualHandler.successCb(this._value);
+    } else {
+      actualHandler.errorCb && actualHandler.errorCb(this._value);
+    }
+  }
+};
+
+
+let funception;
+
+//   $Promise.prototype.then = function (succes,reject){
+//     var obj={
+//         succesCb:succes,
+//         errorCb:reject
+//     }
+//     if(typeof succesCb!=="function"){
+//         obj.succesCb=false
+//     }
+//     if(typeof errorCb!=="function"){
+//         obj.errorCb=false
+//     }
+//     this.handlerGroups.push(obj)
+//   }
 
 module.exports = $Promise;
+
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
 realmente usar module.exports. Pero aquí está para referencia:
